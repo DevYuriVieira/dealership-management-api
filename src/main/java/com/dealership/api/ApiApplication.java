cc824396d9bb4ae1a5f26aa943cf4e15ae2f6d4c
Yuri <yurivieirateixeira@hotmail.com>
2026-05-15 18:41:20 -0300

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/dealership/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.dealership.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dealership/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dealership.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
