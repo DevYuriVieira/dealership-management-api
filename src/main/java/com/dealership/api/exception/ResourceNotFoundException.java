10ccd69a8b9ba246ecfe1e1602e1351d2f020a76
Yuri <yurivieirateixeira@hotmail.com>
2026-05-18 23:48:39 -0300

    Add ResourceNotFoundException for missing records

---
+++ src/main/java/com/dealership/api/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.dealership.api.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
