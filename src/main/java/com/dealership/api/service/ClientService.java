ce99f64561148974e460e8ef37f50bbfa6f35108
Yuri <yurivieirateixeira@hotmail.com>
2026-05-18 22:48:35 -0300

    Add Client repository and refine service logic

--- src/main/java/com/dealership/api/service/ClientService.java
+++ src/main/java/com/dealership/api/service/ClientService.java
@@ -1,9 +1,19 @@
 package com.dealership.api.service;
 
 import com.dealership.api.entity.Client;
+import com.dealership.api.repository.ClientRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ClientService {
+
+    private final ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
     public Client inserir(Client client) {
-        return client;
+        return this.clientRepository.save(client);
     }
 }
