42dd6163c2cb42b98e760e399295f8be1aa0c34e
Yuri <yurivieirateixeira@hotmail.com>
2026-05-22 21:30:43 -0300

    fix: add cascade delete to client vehicles

--- src/main/java/com/dealership/api/entity/Client.java
+++ src/main/java/com/dealership/api/entity/Client.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -29,4 +30,7 @@ public class Client {
 
     @Column(nullable = false)
     private String telefone;
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Vehicle> vehicles;
 }
